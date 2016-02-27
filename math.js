5e691facf77990e72f16c7c00254c7493787d170
Marko Aalto <marko.aalto@reaktor.fi>
2016-02-27 15:15:45 +0200

    Add basic 2d vector math

---
+++ math.js
@@ -0,0 +1,19 @@
+Array.prototype.add = function(other) {
+  return [this[0] + other[0], this[1] + other[1]]
+}
+
+Array.prototype.sub = function(other) {
+  return [this[0] - other[0], this[1] - other[1]]
+}
+
+Array.prototype.mul = function(other) {
+  return Array.isArray(other) ? this[0]*other[0] + this[1]*other[1] : [this[0]*other, this[1]*other]
+}
+
+Array.prototype.norm = function() {
+  return Math.sqrt(this[0]*this[0] + this[1]*this[1])
+}
+
+Array.prototype.unit = function() {
+  return this.mul(1 / this.norm())
+}
