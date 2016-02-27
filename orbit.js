dc9cef3641c14531725bc5a24c5176e394d51441
Marko Aalto <marko.aalto@reaktor.fi>
2016-02-27 15:58:37 +0200

    Add gravity

--- orbit.js
+++ orbit.js
@@ -2,17 +2,19 @@ config = {
   bgColor: '#FF3333',
   fgColor: '#FFFFFF',
   planetRadius: 600,
+  planetPos: [300, 480 + 600 - 100],
+  G: 10000000,
   shipVertices: [[-5, -10], [0, -16], [5, -10], [5, 10], [9, 14], [-9, 14], [-5,10]],
-  maxRuntime: 3,
+  maxRuntime: 5,
 }
 
 initialState = {
   frame: 0,
   time: 0,
-  shipPos: [200, 200],
+  shipPos: [0, 200],
   shipV: [100, 0],
-  shipAngle: 0,
-  shipAngleV: Math.PI*2
+  shipAngle: Math.PI / 2,
+  shipAngleV: Math.PI*2 / 32
 }
 
 window.onload = function() {
@@ -28,11 +30,13 @@ function tick(state, time) {
 
 function update(oldState, newTime) {
   var dt = newTime - oldState.time
+  var altitude = oldState.shipPos.sub(config.planetPos).norm()
+  var gravity = config.planetPos.sub(oldState.shipPos).unit().mul(config.G / (altitude * altitude))
   return {
     time: newTime,
     frame: oldState.frame + 1,
     shipPos: oldState.shipPos.add(oldState.shipV.mul(dt)),
-    shipV: oldState.shipV,
+    shipV: oldState.shipV.add(gravity.mul(dt)),
     shipAngle: oldState.shipAngle + oldState.shipAngleV * dt,
     shipAngleV: oldState.shipAngleV
   }
@@ -46,7 +50,7 @@ function render(state) {
 
   gc.fillStyle = config.fgColor
   gc.beginPath()
-  gc.arc(canvas.width/2, canvas.height + config.planetRadius - 100, config.planetRadius, 0, Math.PI*2)
+  gc.arc(config.planetPos[0], config.planetPos[1], config.planetRadius, 0, Math.PI*2)
   gc.fill()
 
   drawShip(gc, state.shipPos[0], state.shipPos[1], state.shipAngle)
